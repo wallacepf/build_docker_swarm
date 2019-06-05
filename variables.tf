b71335baa232f8177e2dd9b72ac26e4c8a6b33cc
wallacepf <wallacepf@gmail.com>
2019-06-05 19:47:52 -0300

    corrected variables file

--- variables.tf
+++ variables.tf
@@ -2,6 +2,5 @@ variable "esxi_hostname" { default = "192.168.1.100" }
 variable "esxi_hostport" { default = "22" }
 variable "esxi_username" { default = "root" }
 variable "esxi_password" { default = "FNz3qU977BmL" }
-variable "esxi_username" { default = "root" }
 variable "num_mgr" { default = "2"}
 variable "disk_store" { default = "datastore1"}
